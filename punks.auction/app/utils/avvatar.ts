cb5b1584566345c4eb0f64f7b0d03f99712a274e
jalil <jalil@1001.digital>
2026-05-27 22:55:18 +0200

    Change colors again

--- punks.auction/app/utils/avvatar.ts
+++ punks.auction/app/utils/avvatar.ts
@@ -2,7 +2,7 @@ import { avvatarDataUri } from 'avvatars'
 
 export const AVVATAR_GRID_SIZE = 12
 export const AVVATAR_FOREGROUND = '#f7f7f8'
-export const AVVATAR_BACKGROUND = '#ea34b0'
+export const AVVATAR_BACKGROUND = '#f14eba'
 
 export function accountAvvatarDataUri(seed: string, size: number) {
   return avvatarDataUri({
